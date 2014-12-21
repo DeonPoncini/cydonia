a5122ab05fadb7c215461d041ee1b924584a7af5
Deon Poncini <dex1337@gmail.com>
2014-12-20 18:04:32 -0800

    Remove dependency on boost::chrono
    Replace with std::chrono

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,12 +1,12 @@
 #include <network/Utils.h>
 
-#include <boost/chrono.hpp>
+#include <chrono>
 
 time_t currentMs()
 {
-    boost::chrono::milliseconds ms =
-        boost::chrono::duration_cast<boost::chrono::milliseconds>(
-           boost::chrono::high_resolution_clock::now().time_since_epoch());
+    std::chrono::milliseconds ms =
+        std::chrono::duration_cast<std::chrono::milliseconds>(
+           std::chrono::high_resolution_clock::now().time_since_epoch());
     return ms.count();
 }
 
